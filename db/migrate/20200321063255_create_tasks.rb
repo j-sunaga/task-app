2cbc38175aa4239f82f840f717deb78cfaab9890
j-sunaga <124577blue@gmail.com>
2020-03-21 13:58:31 +0700

    taskモデル/の作成

---
+++ db/migrate/20200321063255_create_tasks.rb
@@ -0,0 +1,15 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :task_name
+      t.string :detail
+      t.date :deadline
+      t.string :status
+      t.string :priority
+      t.string :status
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_055634) do
+ActiveRecord::Schema.define(version: 2020_03_21_063255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "task_name"
+    t.string "detail"
+    t.date "deadline"
+    t.string "status"
+    t.string "priority"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_03_21_055634) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
