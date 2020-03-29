13ef5e68601c1250ecbce6616cfe534c1b7156fc
j-sunaga <124577blue@gmail.com>
2020-03-29 15:01:14 +0900

    モデルにバリデーションを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :user,optional: true
+
+  validates :name, presence: true, length: { in: 1..50 }
+  validates :detail, presence: true, length: { in: 1..200 }
+  validates :deadline, presence: true
+  validates :status, presence: true
+  validates :priority, presence: true
+
 end
