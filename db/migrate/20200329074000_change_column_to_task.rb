a1077637c11db5f6b8599b45b3a24d20df796f21
j-sunaga <124577blue@gmail.com>
2020-03-29 16:40:56 +0900

    coloumn変更のマイグレーションファイルの作成

---
+++ db/migrate/20200329074000_change_column_to_task.rb
@@ -0,0 +1,16 @@
+class ChangeColumnToTask < ActiveRecord::Migration[5.2]
+  # 変更内容
+  def up
+    change_column :tasks, :name, :string, null: false, default: ""
+    change_column :tasks, :detail, :string, null: false, default: ""
+    change_column :tasks, :deadline, :date, null: false
+    change_column :tasks, :status, :string, null: false
+    change_column :tasks, :priority, :string, null: false
+    change_column :tasks, :name, :string, limit: 50
+    change_column :tasks, :detail, :string, limit: 200
+  end
+
+  # 変更前の状態
+  def down
+  end
+end
