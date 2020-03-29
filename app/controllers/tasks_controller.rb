2f0512c3924dd30649e509e60c8d9185f077056a
j-sunaga <124577blue@gmail.com>
2020-03-29 14:59:37 +0900

    flashメッセージの日本語化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,7 +24,7 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
 
     if @task.save
-      redirect_to @task, notice: 'Task was successfully created.'
+      redirect_to @task, notice: 'タスクの登録が完了しました'
     else
       render :new
     end
@@ -33,7 +33,7 @@ class TasksController < ApplicationController
   # PATCH/PUT /tasks/1
   def update
     if @task.update(task_params)
-      redirect_to @task, notice: 'Task was successfully updated.'
+      redirect_to @task, notice: 'タスクの更新が完了しました'
     else
       render :edit
     end
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
   # DELETE /tasks/1
   def destroy
     @task.destroy
-    redirect_to tasks_url, notice: 'Task was successfully destroyed.'
+    redirect_to tasks_url, notice: 'タスクを削除しました'
   end
 
   private
