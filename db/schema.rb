094179efdb5e04972d417fa38f521eb75fa08acc
j-sunaga <124577blue@gmail.com>
2020-03-29 12:26:51 +0900

    taskテーブルのtask_nameコラム名をnameに修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_063255) do
+ActiveRecord::Schema.define(version: 2020_03_29_032454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "task_name"
+    t.string "name"
     t.string "detail"
     t.date "deadline"
     t.string "status"
