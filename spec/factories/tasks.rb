fddd97ee83a0327c6ca7a8cf50dadafb3c39813e
j-sunaga <124577blue@gmail.com>
2020-03-29 18:24:59 +0900

    Factoryで代入する値でテスト結果を検証するように修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
     # 下記の内容は実際に作成するカラム名に合わせて変更してください
-    name { 'test_title' }
+    name { 'test_name' }
     detail { 'test_detail' }
     deadline { Random.rand(Date.parse("2020/01/01") .. Date.parse("2020/03/30")) }
     status { 'test_status' }
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe 'タスク管理機能', type: :system do
     context 'タスクを作成した場合' do
       it '作成済みのタスクが表示される' do
        # テストで使用するためのタスクを作成
-       task = create(:task, name: 'task')
+       task = create(:task)
        # タスク一覧ページに遷移
        visit tasks_path
        # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
        # have_contentされているか。（含まれているか。）ということをexpectする（確認・期待する）
-       expect(page).to have_content 'task'
+       expect(page).to have_content 'test_name'
        # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
       end
     end
@@ -48,9 +48,9 @@ RSpec.describe 'タスク管理機能', type: :system do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示されたページに遷移する' do
         #テスト用のタスク作成
-        task = create(:task, name: 'task')
+        task = create(:task)
         visit tasks_path(task)
-        expect(page).to have_content 'task'
+        expect(page).to have_content 'test_name'
        end
      end
   end
