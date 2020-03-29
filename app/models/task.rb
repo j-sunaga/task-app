edeb8168ef06c794eb5d0488e7b3443977a656da
j-sunaga <124577blue@gmail.com>
2020-03-29 19:43:55 +0900

    priorityのデータ型をenumに変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ApplicationRecord
   belongs_to :user,optional: true
 
   enum status: {uncompleted: 0, completed: 1}
-
+  enum priority: {low: 0, middle: 1, high: 2}
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_102135) do
+ActiveRecord::Schema.define(version: 2020_03_29_103937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_03_29_102135) do
     t.string "detail", limit: 200, default: "", null: false
     t.date "deadline", default: "2020-03-29", null: false
     t.integer "status", null: false
-    t.string "priority", null: false
+    t.integer "priority", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
