b31a7342cf3ba54dad91c75730472cf2167cbca1
j-sunaga <124577blue@gmail.com>
2020-04-04 14:46:43 +0900

    ステータスのみで検索した場合とタスク名とステータスで検索した場合のテストを追加

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -46,13 +46,12 @@ RSpec.describe 'タスク管理機能', type: :system do
         task_list = all('.task_row') # タスク一覧を配列として取得するため、View側でidを振っておく
         expect(task_list[0]).to have_content 'high_task'
         expect(task_list[1]).to have_content 'middle_task'
-
       end
     end
     context '検索をした場合' do
       before do
-        create(:task, name: "search_title1")
-        create(:second_task, name: "search_title2")
+        create(:task, name: "search_title1",detail:'search_title1',status:'completed')
+        create(:second_task, name: "search_title2",detail:'search_title2',status:'working')
       end
       it "タイトルで検索できる" do
         visit tasks_path
@@ -61,6 +60,22 @@ RSpec.describe 'タスク管理機能', type: :system do
         # 検索ボタンを押す
         click_on 'commit'
         expect(page).to have_content 'search_title1'
+        expect(page).to have_content 'search_title2'
+      end
+      it "ステータスで検索できる" do
+        visit tasks_path
+        select '完了',from: "status"
+        # 検索ボタンを押す
+        click_on 'commit'
+        expect(page).to have_content 'search_title1'
+      end
+      it "タスク名とステータスで検索できる" do
+        visit tasks_path
+        # タスクの検索欄に検索ワードを入力する (例: task)
+        fill_in "name", with: "search_title"
+        select '着手中',from: "status"
+        # 検索ボタンを押す
+        click_on 'commit'
         expect(page).to have_content 'search_title2'
       end
     end
@@ -73,7 +88,6 @@ RSpec.describe 'タスク管理機能', type: :system do
       # new_task_pathにvisitする（タスク登録ページに遷移する）
       # 1.ここにnew_task_pathにvisitする処理を書く
       visit new_task_path
-
       # 「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄に
       # タスクのタイトルと内容をそれぞれfill_in（入力）する
       # 2.ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
@@ -83,7 +97,6 @@ RSpec.describe 'タスク管理機能', type: :system do
       fill_in "task[deadline]", with: 1.month.ago
       select '未着手',from: "task[status]"
       select '低',from: "task[priority]"
-
       # 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
       # 4.「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
       click_on 'commit'
