39d15f6d6de63fc5ab1ebb992ced8b1156d64661
j-sunaga <124577blue@gmail.com>
2020-04-06 20:28:16 +0900

    newからbuildへの変更

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,35 +1,35 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :model do
   it 'nameが空ならバリデーションが通らない' do
-    task = Task.new(detail: "task_detail",deadline: 1.month.ago, status: :waiting, priority: :low)
+    task = build(:task,name:"")
     expect(task).not_to be_valid
   end
   it 'detailが空ならバリデーションが通らない' do
-    task = Task.new(name: "task_test",deadline: 1.month.ago, status: :waiting, priority: :low)
+    task = build(:task,detail:"")
     expect(task).not_to be_valid
   end
   it 'deadlineが空ならバリデーションが通らない' do
-    task = Task.new(name: "task_test", detail: "task_detail",status: :waiting, priority: :low)
+    task = build(:task,deadline:"")
     expect(task).not_to be_valid
   end
   it 'statusが空ならバリデーションが通らない' do
-    task = Task.new(name: "task_test", detail: "task_detail",deadline: 1.month.ago, priority: :low)
+    task = build(:task,status:"")
     expect(task).not_to be_valid
   end
   it 'priorityが空ならバリデーションが通らない' do
-    task = Task.new(name: "task_test", detail: "task_detail",deadline: 1.month.ago, status: :waiting)
+    task = build(:task,priority:"")
     expect(task).not_to be_valid
   end
   it 'nameが50文字以上ならバリデーションが通らない' do
-    task = Task.new(name: "a"*51, detail: "task_detail",deadline: 1.month.ago, status: :waiting,priority: :low)
+    task = build(:task,name:"a"*51)
     expect(task).not_to be_valid
   end
   it 'detailが200文字以上ならバリデーションが通らない' do
-    task = Task.new(name: "task_test", detail: "t"*201,deadline: 1.month.ago, status: :waiting)
+    task = build(:task,detail:"a"*201)
     expect(task).not_to be_valid
   end
   it '全ての項目に内容が記載されておりname 50文字以内,detail 200文字以内の場合,バリデーションが通る' do
-    task = Task.new(name: "task_test", detail: "task_detail",deadline: 1.month.ago, status: :waiting, priority: :low)
+    task = build(:task,name:"a"*50,detail:"a"*200)
     expect(task).to be_valid
   end
 
