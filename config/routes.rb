21e82c82d47422af93e0c80c2795b084209ef795
j-sunaga <124577blue@gmail.com>
2020-04-08 14:14:38 +0900

    session/newへのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
   resources :users, only: [:new, :create, :show,:edit,:update]
+  resources :sessions, only:[:new,:create,:destroy]
 
 end
