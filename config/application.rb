fdc278d88ed14538962dc6c9aa28338da8f14f58
j-sunaga <124577blue@gmail.com>
2020-04-08 13:58:05 +0900

    rails gの際にcontroller以外のファイルが作られないように設定

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,21 @@ module TaskApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
+
+    config.generators do |g|
+      g.assets     false
+      g.helper     false
+      g.test_framework :rspec,
+      fixtures: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      controller_specs: false,
+      request_specs: false
+      g.jbuilder false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
+
+
   end
 end
