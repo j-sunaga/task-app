0763e8222769dd0f7de351d9adc9a3ec613a60cf
j-sunaga <124577blue@gmail.com>
2020-04-08 12:32:51 +0900

    userコントローラ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,44 @@
 class UsersController < ApplicationController
+
+  before_action :user_find, only: [:show,:edit,:update]
+
+  def new
+     @user = User.new
+  end
+
+  def create
+    @user= User.new(user_params)
+    if @user.save
+      session[:user_id]=@user.id
+      redirect_to user_path(@user), notice: "会員登録が完了しました。"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  #アップデートアクション
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "会員情報を変更しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+
+  def user_find
+    @user= User.find(params[:id])
+  end
+
 end
