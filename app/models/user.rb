107b10f42af67dc4bec51928ee1a0bd2a4beb784
j-sunaga <124577blue@gmail.com>
2020-04-08 13:40:44 +0900

    各コラムにvalidationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   #アソシエーション
   has_many :tasks
 
+  #secure password
   has_secure_password
+
+  #validation
+  before_validation { email.downcase! }
+  validates :name, presence: true, length: {maximum: 30}
+  validates :email, presence: true,  uniqueness: true, length: {maximum: 255},format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password,presence: true, length: { minimum: 6 }
 
 end
