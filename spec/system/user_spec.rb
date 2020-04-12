7c574a74c4b14332295eb45fb4a548ef08409fe7
j-sunaga <124577blue@gmail.com>
2020-04-12 20:10:04 +0900

    userのsystemテストの追加

---
+++ spec/system/user_spec.rb
@@ -0,0 +1,149 @@
+require 'rails_helper'
+
+RSpec.describe 'ユーザ管理機能', type: :system do
+
+  describe 'ユーザ登録機能' do
+    context '必要項目を入力して、createボタンを押した場合' do
+      it 'データが保存される' do
+      visit new_user_path
+      fill_in "user[name]", with: "test_1"
+      fill_in "user[email]", with: "test_1@test.com"
+      fill_in "user[password]", with: "000000"
+      fill_in "user[password_confirmation]", with: "000000"
+      click_on 'commit'
+      expect(page).to have_content 'test_1'
+      end
+    end
+    context 'ログインしていない状態でタスク画面に飛ぼうとした場合' do
+      it 'ログイン画面が表示される' do
+        visit tasks_path
+        expect(page).to have_content 'ログイン'
+      end
+    end
+  end
+  describe 'セッション機能' do
+    context '登録済みのユーザ情報でログインができる' do
+      let!(:user){create(:user)}
+      it 'ログイン後,userページが表示される' do
+        visit new_session_path
+        fill_in "session[email]", with: "#{user.email}"
+        fill_in "session[password]", with: "#{user.password}"
+        click_on 'commit'
+        expect(page).to have_content "#{user.name}"
+      end
+    end
+    context '一般ユーザが他のユーザページアクセスした場合' do
+      let!(:user_1){create(:user)}
+      let!(:user_2){create(:user)}
+      it 'タスク一覧ページに飛ぶ' do
+        #Login
+        visit new_session_path
+        fill_in "session[email]", with: "#{user_1.email}"
+        fill_in "session[password]", with: "#{user_1.password}"
+        click_on 'commit'
+
+        visit user_path(user_2)
+        expect(page).to have_content "タスク一覧"
+      end
+    end
+    context 'ログアウト機能' do
+      let!(:user_1){create(:user)}
+      it "ログアウトをクリックするとログインページ表示される" do
+        visit new_session_path
+        fill_in "session[email]", with: "#{user_1.email}"
+        fill_in "session[password]", with: "#{user_1.password}"
+        click_on 'commit'
+        visit tasks_path
+        click_link 'ログアウト'
+        # 検索ボタンを押す
+        expect(page).to have_content 'ログイン'
+      end
+    end
+  end
+  describe '管理画面のテスト' do
+    # あらかじめタスク一覧のテストで使用するためのタスクを作成する
+    let!(:admin){create(:admin_user)}
+    let!(:user){create(:user)}
+    context '管理者でログインした場合' do
+      it 'ユーザ一覧画面が表示される' do
+        visit new_session_path
+        fill_in "session[email]", with: "#{admin.email}"
+        fill_in "session[password]", with: "#{admin.password}"
+        click_on 'commit'
+        visit admin_users_path
+        click_link '名前でソートする'
+        user_list = all('.user_row')
+        expect(user_list[0]).to have_content "#{admin.name}"
+        expect(user_list[1]).to have_content "#{user.name}"
+      end
+    end
+    context '一般ユーザでログインした場合' do
+      it '管理画面に飛ぼうとするとタスク画面に飛ぶ' do
+        visit new_session_path
+        fill_in "session[email]", with: "#{user.email}"
+        fill_in "session[password]", with: "#{user.password}"
+        click_on 'commit'
+        visit admin_users_path
+        expect(page).to have_content "タスク一覧"
+      end
+    end
+    context '管理者はユーザの新規登録ができる' do
+      it '管理者アカウントでログイン後、ユーザ登録ができる' do
+        visit new_session_path
+        fill_in "session[email]", with: "#{admin.email}"
+        fill_in "session[password]", with: "#{admin.password}"
+        click_on 'commit'
+        visit admin_users_path
+        click_link 'ユーザの登録'
+        fill_in "user[name]", with: "test_user"
+        fill_in "user[email]", with: "test_user@test.com"
+        select '一般ユーザ',from: "user[admin]"
+        fill_in "user[password]", with: "000000"
+        fill_in "user[password_confirmation]", with: "000000"
+        click_on 'commit'
+        visit admin_users_path
+        expect(page).to have_content "test_user"
+      end
+    end
+    context '管理者はユーザの詳細画面にアクセスできる' do
+      it 'ユーザの詳細画面でユーザ名が表示される' do
+        visit new_session_path
+        fill_in "session[email]", with: "#{admin.email}"
+        fill_in "session[password]", with: "#{admin.password}"
+        click_on 'commit'
+        visit admin_users_path
+        visit admin_user_path(user)
+        expect(first('.form-control').value).to eq "#{user.name}"
+      end
+    end
+    context '管理者はユーザ情報を編集できる' do
+      it '編集ページ に飛び、情報を更新される' do
+        visit new_session_path
+        fill_in "session[email]", with: "#{admin.email}"
+        fill_in "session[password]", with: "#{admin.password}"
+        click_on 'commit'
+        visit admin_users_path
+        visit edit_admin_user_path(user)
+        fill_in "user[name]", with: "test_user"
+        fill_in "user[password]", with: "000000"
+        fill_in "user[password_confirmation]", with: "000000"
+        click_on 'commit'
+        visit admin_users_path
+        expect(page).to have_content "test_user"
+      end
+    end
+    context '管理者はユーザを削除できる' do
+      it '削除をクリックするとuser情報が表示されない' do
+        visit new_session_path
+        fill_in "session[email]", with: "#{admin.email}"
+        fill_in "session[password]", with: "#{admin.password}"
+        click_on 'commit'
+        visit admin_users_path
+        find_by_id('1').click_link
+        page.accept_confirm "本当に削除してもよろしいですか？"
+        visit admin_users_path
+        expect(page).not_to have_content "#{user.name}"
+      end
+    end
+  end
+end
