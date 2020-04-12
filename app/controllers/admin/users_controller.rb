73bdf5142ff90770f41025bcd08f8e30a6a14264
j-sunaga <124577blue@gmail.com>
2020-04-12 11:23:53 +0900

    各ファイルの遷移先パスの修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -19,8 +19,7 @@ class Admin::UsersController < ApplicationController
   def create
     @user= User.new(user_params)
     if @user.save
-      session[:user_id]=@user.id
-      redirect_to user_path(@user), notice: "会員登録が完了しました。"
+      redirect_to admin_user_path(@user), notice: "会員登録が完了しました。"
     else
       render 'new'
     end
@@ -57,7 +56,7 @@ class Admin::UsersController < ApplicationController
   def admin_check
     if session[:user_id] != params[:id].to_i
       flash[:danger] = '別のユーザ情報は確認できません'
-      redirect_to tasks_path
+      redirect_to users_path
     end
   end
 
