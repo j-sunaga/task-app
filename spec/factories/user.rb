b325dc9a94872553afdf97cceffc045486dc45f5
j-sunaga <124577blue@gmail.com>
2020-04-12 16:44:20 +0900

    userモデルのFactoryBotにadmin_userを追加

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,16 +3,24 @@ FactoryBot.define do
   # 作成するテストデータの名前を「user」とします
   # （実際に存在するクラス名と一致するテストデータの名前をつければ、そのクラスのテストデータを自動で作成します）
   factory :user do
-
     sequence :name do |n|
       "test_#{n+1}"
     end
-
     sequence :email do |n|
       "test_#{n+1}@test.com"
     end
+    password {'000000'}
+    admin{ false }
+  end
 
-    password {'123456'}
-
+  factory :admin_user, class: User do
+    sequence :name do |n|
+      "admin_test_#{n+1}"
+    end
+    sequence :email do |n|
+      "admin_test_#{n+1}@test.com"
+    end
+    password {'000000'}
+    admin{ true }
   end
 end
