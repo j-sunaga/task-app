c0247c02e0d1c84674335e6dbfcfde0f8c24c736
j-sunaga <124577blue@gmail.com>
2020-04-15 15:43:11 +0900

    labelsテーブルにuser_idの外部参照を設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_045403) do
+ActiveRecord::Schema.define(version: 2020_04_15_064102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2020_04_15_045403) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_labels_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -51,5 +53,6 @@ ActiveRecord::Schema.define(version: 2020_04_15_045403) do
     t.boolean "admin", default: false, null: false
   end
 
+  add_foreign_key "labels", "users"
   add_foreign_key "tasks", "users"
 end
