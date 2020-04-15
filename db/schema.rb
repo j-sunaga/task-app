b031be177999ae3800381ed5c7c00292bc0f0715
j-sunaga <124577blue@gmail.com>
2020-04-15 14:16:13 +0900

    labelingsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_043206) do
+ActiveRecord::Schema.define(version: 2020_04_15_045403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labelings", force: :cascade do |t|
+    t.integer "task_id"
+    t.integer "label_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "labels", force: :cascade do |t|
     t.string "name"
