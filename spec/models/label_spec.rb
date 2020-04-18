2002843b71eb957a7e0f657505f58ce6a7d8e4c3
j-sunaga <124577blue@gmail.com>
2020-04-18 19:03:57 +0900

    not_to validからto invalidに修正

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe Label, type: :model do
 
   it "nameが空ならバリデーションが通らない" do
     label = build(:label,name:"")
-    expect(label).not_to be_valid
+    expect(label).to be_invalid
   end
 
   it "nameが50以上ならバリデーションが通らない" do
     label = build(:label,name:"a"*51)
-    expect(label).not_to be_valid
+    expect(label).to be_invalid
   end
 
   it "nameが50以下ならバリデーションが通る" do
