b14cc41fde96785d4829307f78f98f19ecbc0b77
j-sunaga <124577blue@gmail.com>
2020-04-24 16:44:44 +0900

    同名のlabelを保存できないようにするための条件をカスタムメソッドとして定義

--- app/models/label.rb
+++ app/models/label.rb
@@ -6,4 +6,20 @@ class Label < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :name, presence: true, length: { in: 1..50 }
+  validate :cannot_save_same_label
+
+  def cannot_save_same_label
+    # update
+    if id.present?
+      name_duplicate_error if Label.find(id).name != name
+    # create
+    else
+      name_duplicate_error
+    end
+  end
+
+  def name_duplicate_error
+    msg = ': 同じ名前のラベルを保存することはできません'
+    errors.add(:name, msg) if Label.where(user: user_id, name: name).present?
+  end
 end
