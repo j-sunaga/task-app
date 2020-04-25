42d717e594c9654807b36de08f63e884e3970720
j-sunaga <124577blue@gmail.com>
2020-04-25 12:48:01 +0900

    basic認証の解除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  if Rails.env == 'production'
-    http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
-  end
-
   include SessionsHelper
 end
